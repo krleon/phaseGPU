4f0d3e47ec1c5310e21cc65a356d515e37ea4885
krleon <Kevin@KEVINs-MacBook-Air.local>
2015-09-30 22:26:40 -0400

    Added structure function

--- arrayFire_test.cpp
+++ arrayFire_test.cpp
@@ -24,12 +24,22 @@ af::array getPSDArray(float D, float r0, float L0, float l0, int N) {
 
 }
 
-af::array ift2(af::array in) {
+af::array ift2(af::array in, float delta_f) {
+
+	int N = in.dims(0);
 
 	af::array temp = af::shift(in, (in.dims(0)+1)/2, (in.dims(1)+1)/2);
 	af::ifft2InPlace(temp);
 
-	return af::shift(temp, (temp.dims(0)+1)/2, (temp.dims(1)+1)/2);
+	return af::shift(temp, (temp.dims(0)+1)/2, (temp.dims(1)+1)/2)*powf(N*delta_f,2);
+
+}
+
+af::array ft2(af::array in, float delta) {
+
+	af::array temp = af::shift(in, in.dims(0)/2, in.dims(1)/2);
+	af::fft2InPlace(temp);
+	return af::shift(temp, temp.dims(0)/2, temp.dims(1)/2)*powf(delta,2);
 
 }
 
@@ -38,6 +48,19 @@ af::array getRandom(int NX, int NY, int NZ) {
 	af::array b = af::randn(NX, NY, NZ);
 
 	return af::complex(a,b);
+}
+
+af::array calculateStrutureFn(af::array ph, af::array mask, float delta) {
+
+	af::array P = ft2(ph*mask, delta);
+	af::array S = ft2(af::pow(ph*mask), delta);
+	af::array W = ft2(mask, delta);
+
+	float delta_f = 1/(ph.dim(0)*delta);
+	af::array w2 = ift2(W*af::conjg(W), delta_f);
+
+	return (mask*2*ift2(af::real(S*conjg(W)) - af::pow(af::abs(P)), delta_f) / w2);
+
 }
 
 int main(void) {
@@ -61,9 +84,12 @@ int main(void) {
 	af::array out(N,N,num_screens);
 
 	gfor (af::seq i, num_screens) {
-		out(af::span,af::span,i) = af::real(ift2(c(af::span,af::span,i)))*N*N;	
+		out(af::span,af::span,i) = af::real(ift2(c(af::span,af::span,i),1));	
 	}
 	
+	af::array mask = af::constant(0.0, N, N);
+	af::array strfn = calculateStrutureFn(out(af::span,af::span,0), mask, D/N);
+
 	//print min/max
 	float minVal = af::min<float>(out(af::span,af::span,0));
 	float maxVal = af::max<float>(out(af::span,af::span,0));
