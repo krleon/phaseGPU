ab1178fece339358e5c1583ea3b8f81a4fb60025
krleon <Kevin@KEVINs-MacBook-Air.local>
2015-09-30 20:39:06 -0400

    updated to 3D array

--- arrayFire_test.cpp
+++ arrayFire_test.cpp
@@ -1,6 +1,7 @@
 #include <arrayfire.h>
 #include <math.h>
 #include <stdio.h>
+#include <time.h>
 
 af::array getPSDArray(float D, float r0, float L0, float l0, int N) {
 
@@ -17,14 +18,32 @@ af::array getPSDArray(float D, float r0, float L0, float l0, int N) {
 	float f0 = 1.0/L0;
 
 	af::array PSD_phi = 0.023*powf(r0,-5./3.)*af::exp(-af::pow((f/fm),2))/af::pow(af::pow(f,2)+powf(f0,2),11./6.);
+	PSD_phi(N/2,N/2) = 0.0;
 
 	return PSD_phi;
 
 }
 
+af::array ift2(af::array in) {
+
+	af::array temp = af::shift(in, (in.dims(0)+1)/2, (in.dims(1)+1)/2);
+	af::ifft2InPlace(temp);
+
+	return af::shift(temp, (temp.dims(0)+1)/2, (temp.dims(1)+1)/2);
+
+}
+
+af::array getRandom(int NX, int NY, int NZ) {
+	af::array a = af::randn(NX, NY, NZ);
+	af::array b = af::randn(NX, NY, NZ);
+
+	return af::complex(a,b);
+}
+
 int main(void) {
 
-	int N = 4096;
+	int N = 512;
+	int num_screens = 100;
 	
 	float D = 2.0;
 	float r0 = 0.1;
@@ -34,44 +53,28 @@ int main(void) {
 	af::setDevice(0);
 	af::info();
 
-	af::array PSD_phi = getPSDArray(D, r0, L0, l0, N);
-	PSD_phi(N/2,N/2) = 0.0;
+	af::array PSD_phi = af::tile(getPSDArray(D, r0, L0, l0, N),1,1,num_screens);
 
-	af::array out;
+	af::setSeed(time(NULL));
+	af::array c = getRandom(N, N, num_screens)*af::sqrt(PSD_phi)/D;
 
-	gfor (af::seq i, 10) {
-	af::array a = af::randn(N, N)*af::sqrt(PSD_phi)/D;
-	af::array b = af::randn(N, N)*af::sqrt(PSD_phi)/D;
-	//af::array a = af::randn(2,2);
-	//af::array b = af::randn(2,2);
-	
-	
-	af::array c = af::complex(a,b);
+	af::array out(N,N,num_screens);
 
-	//shift
-	af::array shift_out = af::shift(c, (c.dims(0)+1)/2, (c.dims(1)+1)/2);
-	//ifft
-	ifft2InPlace(shift_out);
-	//shift
-	c = af::shift(shift_out, (shift_out.dims(0)+1)/2, (shift_out.dims(1)+1)/2);
-	//get real
-	
-	out = af::real(c)*N*N;
-}
-	//show
+	gfor (af::seq i, num_screens) {
+		out(span,span,i) = af::real(ift2(c(span,span,i)))*N*N;	
+	}
 	
 	//print min/max
-	float minVal = af::min<float>(out);
-	float maxVal = af::max<float>(out);
+	float minVal = af::min<float>(out(span,span,0));
+	float maxVal = af::max<float>(out(span,span,0));
 	printf("Minimum Val: %f\n", minVal);
 	printf("Maximum Val: %f\n", maxVal);
 
 	af::Window wnd(N, N, "Phase Screen");
 	//wnd.setColorMap(AF_COLORMAP_SPECTRUM);
    	//af::print("out",(out-minVal)/(maxVal-minVal));
-	af::array out2 = (out-minVal)/(maxVal-minVal);
+	af::array out2 = (out(span,span,0)-minVal)/(maxVal-minVal);
 
-	af::print("out",out(1,af::seq(1,10)),10);
 	while(!wnd.close()) {
 		wnd.image(out2);
 		wnd.show();
