4099d9932ac046877b76ce5a1bd1fa23a836d765
kevin <krleon@yahoo.com>
2015-10-01 00:26:06 -0400

    structure function with working plot in ArrayFire

--- arrayFire_test.cpp
+++ arrayFire_test.cpp
@@ -9,9 +9,6 @@ af::array getPSDArray(float D, float r0, float L0, float l0, int N) {
 	af::array y = af::tile(b,1,N);
 	af::array x = af::tile(b.T(),N,1);
 
-	//af::print("x",x(af::seq(0,9)),0);
-	//af::print("y",y(0,af::seq(0,9)));
-
 	af::array f = af::sqrt(af::pow(x,2) + af::pow(y,2));
 
 	float fm = 5.92/l0/(2.0*af::Pi);
@@ -50,16 +47,16 @@ af::array getRandom(int NX, int NY, int NZ) {
 	return af::complex(a,b);
 }
 
-af::array calculateStrutureFn(af::array ph, af::array mask, float delta) {
+af::array calculateStructureFn(af::array ph, af::array mask, float delta) {
 
-	af::array P = ft2(ph*mask, delta);
-	af::array S = ft2(af::pow(ph*mask,2), delta);
-	af::array W = ft2(mask, delta);
+	af::array P = ft2(af::complex(ph*mask), delta);
+	af::array S = ft2(af::complex(af::pow(ph*mask,2)), delta);
+	af::array W = ft2(af::complex(mask), delta);
 
 	float delta_f = 1/(ph.dims(0)*delta);
 	af::array w2 = ift2(W*af::conjg(W), delta_f);
 
-	return (mask*2*ift2(af::real(S*conjg(W)) - af::pow(af::abs(P),2), delta_f) / w2);
+	return (mask*2*ift2(af::complex(af::real(S*conjg(W))) - af::pow(af::abs(P),2), delta_f) / w2);
 
 }
 
@@ -67,9 +64,9 @@ int main(void) {
 
 	int N = 512;
 	int num_screens = 40;
-	
-	float D = 2.0;
-	float r0 = 0.1;
+
+	float D = 6.0;
+	float r0 = 0.2;
 	float L0 = 40;
 	float l0 = 0.01;
 
@@ -82,27 +79,44 @@ int main(void) {
 	af::array c = getRandom(N, N, num_screens)*af::sqrt(PSD_phi)/D;
 
 	af::array out(N,N,num_screens);
+	af::array avgstrfn = af::constant(0.0,N,N);
 
 	gfor (af::seq i, num_screens) {
-		out(af::span,af::span,i) = af::real(ift2(c(af::span,af::span,i),1));	
+		out(af::span,af::span,i) = af::real(ift2(c(af::span,af::span,i),1));
 	}
-	
-	af::array mask = af::constant(0.0, N, N);
-	af::array strfn = calculateStrutureFn(out(af::span,af::span,0), mask, D/N);
+
+	af::array b = af::array(af::seq(-N/2,N/2-1))*D/N;
+	af::array y = af::tile(b,1,N);
+	af::array x = af::tile(b.T(),N,1);
+        af::array f = af::sqrt(af::pow(x,2) + af::pow(y,2));
+
+	af::array mask = af::constant(0.0,N,N);
+	mask(af::where(f < .85*(D/2.))) = 1.0;
+	//gfor (af::seq i, num_screens)
+	//	fullstrfn(af::span,af::span,i) = calculateStructureFn(out(af::span,af::span,i),mask,D/N);
+
+	for (int ii = 0; ii < num_screens; ii++) {
+		avgstrfn = avgstrfn + calculateStructureFn(out(af::span, af::span, ii), mask, D/N);
+	}
+
+	avgstrfn = avgstrfn/num_screens;
 
 	//print min/max
-	float minVal = af::min<float>(out(af::span,af::span,0));
-	float maxVal = af::max<float>(out(af::span,af::span,0));
+	float minVal = af::min<float>(avgstrfn);
+	float maxVal = af::max<float>(avgstrfn);
 	printf("Minimum Val: %f\n", minVal);
 	printf("Maximum Val: %f\n", maxVal);
 
 	af::Window wnd(N, N, "Phase Screen");
-	//wnd.setColorMap(AF_COLORMAP_SPECTRUM);
-   	//af::print("out",(out-minVal)/(maxVal-minVal));
-	af::array out2 = (out(af::span,af::span,0)-minVal)/(maxVal-minVal);
+	af::array out3 = (af::abs(avgstrfn)-minVal)/(maxVal-minVal);
+
+	af::array yaxes = af::abs(avgstrfn(255,af::seq(256,511)));
+	af::array xaxes = af::seq(0,N/2-1);;
+	xaxes = xaxes*D/(N*r0);
 
 	while(!wnd.close()) {
-		wnd.image(out2);
+		//af::array y = avgstrfn(255,af::seq(256,511));
+		wnd.plot(xaxes,yaxes.T());
 		wnd.show();
 	}
     return 0;
